e1bf0f4ff30129564f846845316a2c9f32ae10df
danyentezari <dany.entezari@gmail.com>
2021-12-11 13:34:29 +0400

    Making NavBar stateful with useEffect.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import LayoutRoute from "./LayoutRoute.js";
 import MainScreen from './MainScreen.js';
+import RegistrationScreen from './RegistrationScreen.js';
 
 function App() {
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact={true} component={MainScreen} />
+                <LayoutRoute path="/" exact={true} component={MainScreen} />
+                <LayoutRoute path="/register" exact={true} component={RegistrationScreen} />
             </Switch>
         </BrowserRouter>
     )
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,51 @@
-import logo from './logo.svg'
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import logo from './logo.svg';
 
-function NavBar() {
+function NavBar(props) {
+
+    console.log( props.path )
+
+    const initialState = {
+        'home': '',
+        'register': ''
+    }
+    const [state, setState] = useState(initialState)
+
+
+    useEffect(
+        function() {
+
+            if(props.path === '/') {
+                setState(
+                    {...initialState, 'home': 'active'}
+                )
+            } else  if(props.path === '/register') {
+                setState(
+                    {...initialState, 'register': 'active'}
+                )
+            }
+        },
+        [props.path]
+    )
+
+
+
     return (
         <div className="container">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-            <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                 <img src={logo} alt="logo" width="64" /> 
                 <span className="fs-4">Simple header</span>
-            </a>
+            </Link>
 
             <ul className="nav nav-pills">
-                <li className="nav-item"><a href="http://localhost:3000" className="nav-link active">Home</a></li>
-                <li className="nav-item"><a href="http://localhost:3000" className="nav-link">Features</a></li>
-                <li className="nav-item"><a href="http://localhost:3000" className="nav-link">Pricing</a></li>
-                <li className="nav-item"><a href="http://localhost:3000" className="nav-link">FAQs</a></li>
-                <li className="nav-item"><a href="http://localhost:3000" className="nav-link">About</a></li>
+                <li className="nav-item">
+                    <Link to="/" className={`nav-link ${state['home']}`}>Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/register" className={`nav-link ${state['register']}`}>Register</Link>
+                </li>
             </ul>
             </header>
         </div>
